7f7f8e89a51debb7d00be1ab2876a4887a7f75e3
MicrexG <johnmichael.villaraza@uap.asia>
2023-09-17 13:39:23 +0800

    please be the errror

--- twitter/controllers/fetchUser.js
+++ twitter/controllers/fetchUser.js
@@ -1,24 +1,26 @@
 const userDetailsConfig = {
-    "api": {
-    "uri": `https://twitter154.p.rapidapi.com/user/details?username=`,
-    "method": "GET",
-    "headers": {
-    "X-RapidAPI-Key": "<https://rapidapi.com/omarmhaimdat/api/twitter154/>",
-    "X-RapidAPI-Host": "<twitter154.p.rapidapi.com>"
-    }
-    }
-   }
-   async function getUserDetails(username) {
-    const url = `${userDetailsConfig.api.uri}${username}`;
-    console.log(url);
-    const options = {
+  api: {
+    uri: "https://twitter154.p.rapidapi.com/user/details?username=",
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "YOUR_RAPIDAPI_KEY_HERE",
+      "X-RapidAPI-Host": "twitter154.p.rapidapi.com",
+    },
+  },
+};
+
+async function getUserDetails(username) {
+  const url = `${userDetailsConfig.api.uri}${username}`;
+  console.log(url);
+  const options = {
     method: userDetailsConfig.api.method,
-    headers: userDetailsConfig.api.headers
-    };
-    const response = await fetch(url, options);
-    return addClientGeneratedProperties(await response.json());
-   };
-   function addClientGeneratedProperties(data){
-    data.twitter_link = `https://twitter.com/${data.username}`;
-    return data;
-   }
+    headers: userDetailsConfig.api.headers,
+  };
+  const response = await fetch(url, options);
+  return addClientGeneratedProperties(await response.json());
+}
+
+function addClientGeneratedProperties(data) {
+  data.twitter_link = `https://twitter.com/${data.username}`;
+  return data;
+}
